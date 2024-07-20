6b8cdd99faf49746c25df0e173c9a61c7f7bf7e5
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2024-07-20 07:11:43 +0100

    animation

---
+++ src/components/navLink.jsx
@@ -0,0 +1,12 @@
+"use clinet"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({link})=>{
+const pathName=usePathname();
+console.log(pathName);
+    return(
+        <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white text-xl"}`} href={link.url}>{link.title} </Link>
+    )
+}
+export default NavLink;
